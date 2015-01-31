38a4eda7fe013c5c4217a562b9d8ccb36483c82d
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2015-01-31 16:17:16 +0100

    Support for double-dual spaces.

--- Data/LinearMap/HerMetric.hs
+++ Data/LinearMap/HerMetric.hs
@@ -6,14 +6,19 @@
 {-# LANGUAGE UndecidableInstances       #-}
 {-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 
 
 
 
 module Data.LinearMap.HerMetric (
-    HerMetric
-  , projector
-  , metricSq, metric, metrics
+  -- * Metric operator types
+    HerMetric, HerMetric'(..)
+  -- * Evaluating metrics
+  , metricSq, metricSq', metric, metric', metrics, metrics'
+  -- * Defining metrics by projectors
+  , projector, projector'
+  -- * Utility
   , metriScale
   , adjoint
   , transformMetric
@@ -49,6 +54,14 @@ infixr 7 <.>^, ^<.>
 --   quantum mechanics), /standard range of statistical fluctuations/, and /volume element/.
 newtype HerMetric v = HerMetric { getHerMetric :: v :-* DualSpace v }
 
+
+-- | A metric on the dual space; equivalent to a linear mapping from the dual space
+--   to the original vector space.
+-- 
+--   Prime-versions of the functions in this module target those dual-space metrics, so
+--   we can avoid some explicit handling of double-dual spaces.
+newtype HerMetric' v = HerMetric' { dualMetric :: HerMetric (DualSpace v) }
+
 instance HasMetric v => AdditiveGroup (HerMetric v) where
   zeroV = HerMetric zeroV
   negateV (HerMetric m) = HerMetric $ negateV m
@@ -62,11 +75,14 @@ instance HasMetric v => VectorSpace (HerMetric v) where
 --   
 --   It will perhaps be the most common way of defining 'Hermetric' values to start
 --   with such dual-space vectors and superimpose the projectors using the 'VectorSpace'
---   instance; e.g. @projector (1,0) '^+^' projector (0,2)@ yields a hermitian operator
+--   instance; e.g. @'projector' (1,0) '^+^' 'projector' (0,2)@ yields a hermitian operator
 --   describing the ellipsoid span of the vectors /e/&#x2080; and 2&#x22c5;/e/&#x2081;.
 --   Metrics generated this way are always positive definite.
 projector :: HasMetric v => DualSpace v -> HerMetric v
 projector u = HerMetric (linear $ \v -> u ^* (u<.>^v))
+
+projector' :: (HasMetric v, HasMetric (DualSpace v)) => v -> HerMetric' v
+projector' v = HerMetric' . HerMetric . linear $ \u -> doubleDual $ v ^* (doubleDual v<.>^u)
 
 
 
@@ -75,12 +91,20 @@ projector u = HerMetric (linear $ \v -> u ^* (u<.>^v))
 metricSq :: HasMetric v => HerMetric v -> v -> Scalar v
 metricSq (HerMetric m) v = lapply m v <.>^ v
 
--- | Evaluate a vector's magnitude through a metric. This assumes an actual mathematical
---   metric, i.e. positive definite &#x2013; otherwise the internally used square root may
---   get negative arguments (though it can still produce results if the scalars are complex).
+metricSq' :: forall v . (HasMetric v, HasMetric (DualSpace v))
+                     => HerMetric' v -> DualSpace v -> Scalar v
+metricSq' (HerMetric' (HerMetric m)) u = (unDoubleDual (lapply m u) :: v) ^<.> u
+
+-- | Evaluate a vector's &#x201c;magnitude&#x201d; through a metric. This assumes an actual
+--   mathematical metric, i.e. positive definite &#x2013; otherwise the internally used
+--   square root may get negative arguments (though it can still produce results if the
+--   scalars are complex; however, complex spaces aren't supported yet).
 metric :: HasMetric v => HerMetric v -> v -> Scalar v
 metric (HerMetric m) v = sqrt $ lapply m v <.>^ v
 
+metric' :: forall v . (HasMetric v, HasMetric (DualSpace v))
+                        => HerMetric' v -> DualSpace v -> Scalar v
+metric' (HerMetric' (HerMetric m)) u = sqrt $ (unDoubleDual (lapply m u) :: v) ^<.> u
 
 metriScale :: HasMetric v => HerMetric v -> v -> v
 metriScale m v = metric m v *^ v
@@ -94,6 +118,9 @@ metriScale m v = metric m v *^ v
 metrics :: HasMetric v => HerMetric v -> [v] -> Scalar v
 metrics m vs = sqrt . sum $ metricSq m <$> vs
 
+metrics' :: forall v . (HasMetric v, HasMetric (DualSpace v))
+                          => HerMetric' v -> [DualSpace v] -> Scalar v
+metrics' m vs = sqrt . sum $ metricSq' m <$> vs
 
 
 transformMetric :: (HasMetric v, HasMetric w, Scalar v ~ Scalar w)
@@ -109,12 +136,14 @@ class ( HasBasis v, RealFloat (Scalar v), HasTrie (Basis v)
       , Scalar v ~ Scalar (DualSpace v), Basis v ~ Basis (DualSpace v) )
     => HasMetric v where
         
-  -- | @'DualSpace' v@ is isomorphic to @v :-* Scalar v@. Typically (for all Hilbert- /
-  --   'InnerSpace's) this is in turn isomorphic to @v@ itself, which will be rather more
-  --   efficient (hence the distinction between a vector space and its dual is often
-  --   neglected or reduced to &#x201c;column vs row vectors&#x201d;. Mathematically though,
-  --   it makes sense to keep the concepts apart, even if ultimately @'DualSpace' v ~ v@
-  --   (which need /not/ always be the case, though!).
+  -- | @'DualSpace' v@ is isomorphic to the space of linear functionals on @v@, i.e.
+  --   @v ':-*' 'Scalar' v@.
+  --   Typically (for all Hilbert- / 'InnerSpace's) this is in turn isomorphic to @v@
+  --   itself, which will be rather more efficient (hence the distinction between a
+  --   vector space and its dual is often neglected or reduced to &#x201c;column vs row
+  --   vectors&#x201d;).
+  --   Mathematically though, it makes sense to keep the concepts apart, even if ultimately
+  --   @'DualSpace' v ~ v@ (which needs not /always/ be the case, though!).
   type DualSpace v :: *
   type DualSpace v = v
       
@@ -125,6 +154,14 @@ class ( HasBasis v, RealFloat (Scalar v), HasTrie (Basis v)
   --   (completely unchecked) that the supplied function is linear.
   functional :: (v -> Scalar v) -> DualSpace v
   
+  -- | While isomorphism between a space and its dual isn't generally canonical,
+  --   the /double-dual/ space should be canonically isomorphic in pretty much
+  --   all relevant cases. Indeed, it is recommended that they are the very same type;
+  --   the tuple instance actually assumes this to be able to offer an efficient
+  --   implementation (namely, 'id') of the isomorphisms.
+  doubleDual :: HasMetric (DualSpace v) => v -> DualSpace (DualSpace v)
+  unDoubleDual :: HasMetric (DualSpace v) => DualSpace (DualSpace v) -> v
+  
   
 
 -- | Simple flipped version of '<.>^'.
@@ -134,10 +171,17 @@ ket ^<.> bra = bra <.>^ ket
 instance HasMetric Double where
   (<.>^) = (<.>)
   functional f = f 1
-instance (HasMetric v, HasMetric w, Scalar v ~ Scalar w) => HasMetric (v,w) where
+  doubleDual = id
+  unDoubleDual = id
+instance ( HasMetric v, HasMetric w, Scalar v ~ Scalar w
+         , HasMetric (DualSpace v), DualSpace (DualSpace v) ~ v
+         , HasMetric (DualSpace w), DualSpace (DualSpace w) ~ w
+         ) => HasMetric (v,w) where
   type DualSpace (v,w) = (DualSpace v, DualSpace w)
   (v,w)<.>^(v',w') = v<.>^v' + w<.>^w'
   functional f = (functional $ f . (,zeroV), functional $ f . (zeroV,))
+  doubleDual = id
+  unDoubleDual=id
 
 
 
