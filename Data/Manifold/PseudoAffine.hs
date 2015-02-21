97d94aa1771bae763c8559e1579043f77de4f2de
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2015-02-21 02:45:23 +0100

    Some documentation on differentiable categories.

--- Data/Manifold/PseudoAffine.hs
+++ Data/Manifold/PseudoAffine.hs
@@ -37,9 +37,10 @@
 module Data.Manifold.PseudoAffine (
             -- * Manifold class
               PseudoAffine(..)
+            -- * Regions within a manifold
+            , Region
             -- * Hierarchy of manifold-categories
             , Differentiable
-            , Region
             , PWDiffable, RWDiffable
             ) where
     
@@ -183,6 +184,39 @@ dev_ε_δ :: RealDimension a
                 => (a -> a) -> LinDevPropag a a
 dev_ε_δ f d = let ε = 1 / metric d 1 in projector $ 1 / sqrt (f ε)
 
+-- | The category of differentiable functions between manifolds over scalar @s@.
+--   
+--   As you might guess, these offer /automatic differentiation/ of sorts (basically,
+--   simple forward AD), but that's in itself is not really the killer feature here.
+--   More interestingly, we actually have the (à la Curry-Howard) /proof/
+--   built in: the function /f/ has at /x/&#x2080; derivative /f'&#x2093;/&#x2080;,
+--   if, for&#xb9; /&#x3b5;/>0, there exists /&#x3b4;/ such that
+--   |/f/ /x/ &#x2212; (/f/ /x/&#x2080; + /x/&#x22c5;/f'&#x2093;/&#x2080;)| < /&#x3b5;/
+--   for all |/x/ &#x2212; /x/&#x2080;| < /&#x3b4;/.
+-- 
+--   Observe that, though this looks quite similar to the standard definition
+--   of differentiability, it is not equivalent thereto &#x2013; in fact it does
+--   not prove any analytic properties at all. To make it equivalent, we need
+--   a lower bound on /&#x3b4;/: simply /&#x3b4;/ gives us continuity, and for
+--   continuous differentiability, /&#x3b4;/ must grow at least like &#x221a;/&#x3b5;/
+--   for small /&#x3b5;/. Neither of these conditions are enforced by the type system,
+--   but we do require them for any allowed values because these proofs are obviously
+--   tremendously useful &#x2013; for instance, you can have a root-finding algorithm
+--   and actually be sure you get /all/ solutions correctly, not just /some/ that are
+--   (hopefully) the closest to some reference point you'd need to laborously define!
+-- 
+--   Unfortunately however, this also prevents doing any serious algebra etc. with the
+--   category, because even something as simple as division necessary introduces singularities
+--   where the derivatives must diverge.
+--   Not to speak of many trigonometric e.g. trigonometric functions that
+--   are undefined on whole regions. The 'PWDiffable' and 'RWDiffable' categories have explicit
+--   handling for those issues built in; you may simply use these categories even when
+--   you know the result will be smooth in your relevant domain (or must be, for e.g.
+--   physics reasons).
+--   
+--   &#xb9;(The implementation does not deal with /&#x3b5;/ and /&#x3b4;/ as difference-bounding
+--   reals, but rather as metric tensors that define a boundary by prohibiting the
+--   overlap from exceeding one; this makes the concept actually work on general manifolds.)
 newtype Differentiable s d c
    = Differentiable { runDifferentiable ::
                         d -> ( c, PseudoDiff d :-* PseudoDiff c, LinDevPropag d c ) }
@@ -628,6 +662,24 @@ instance (RealDimension n, LocallyScalable n a)
 -- | Category of functions that, where defined, have an open region in
 --   which they are continuously differentiable. Hence /RegionWiseDiff'able/.
 --   Basically these are the partial version of `PWDiffable`.
+-- 
+--   Though the possibility of undefined regions is of course not too nice
+--   (we don't need Java to demonstrate this with its everywhere-looming @null@ values...),
+--   this category will propably be the &#x201c;workhorse&#x201d; for most serious
+--   calculus applications, because it contains all the usual trig etc. functions
+--   and of course everything algebraic you can do in the reals.
+-- 
+--   The easiest way to define ordinary functions in this category is hence
+--   with its 'AgentVal'ues, which have instances of the standard classes 'Num'
+--   through 'Floating'. For instance, the following defines the /binary entropy/
+--   as a differentiable function on the interval @]0,1[@: (it will
+--   actually /know/ where it's defined and where not! &#x2013; and I don't mean you
+--   need to exhaustively 'isNaN'-check all results...)
+-- 
+-- @
+-- hb :: RWDiffable R R R
+-- hb = alg (\\p -> - p * logBase 2 p - (1-p) * logBase 2 (1-p) )
+-- @
 newtype RWDiffable s d c
    = RWDiffable {
         tryDfblDomain :: d -> (PreRegion s d, Option (Differentiable s d c)) }
