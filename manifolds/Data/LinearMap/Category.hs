928151861bf342c6532fef6a1069367b34141a4a
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2015-04-21 15:22:41 +0200

    Start proper implementation of linear-map category.

---
+++ manifolds/Data/LinearMap/Category.hs
@@ -0,0 +1,62 @@
+-- |
+-- Module      : Data.LinearMap.Category
+-- Copyright   : (c) Justus Sagemüller 2015
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveFoldable             #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE UnicodeSyntax              #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE PatternGuards              #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE DataKinds                  #-}
+
+module Data.LinearMap.Category where
+
+import Data.Tagged
+import Data.Semigroup
+
+import Data.MemoTrie
+import Data.VectorSpace
+import Data.AffineSpace
+import Data.Basis
+import Data.AdditiveGroup
+    
+import qualified Prelude as Hask hiding(foldl)
+import qualified Control.Applicative as Hask
+import qualified Control.Monad       as Hask
+import qualified Data.Foldable       as Hask
+
+
+import Control.Category.Constrained.Prelude hiding ((^))
+import Control.Arrow.Constrained
+
+
+import qualified Data.Vector as Arr
+import qualified Numeric.LinearAlgebra.HMatrix as HMat
+
+
+    
+-- | A linear mapping between finite-dimensional spaces, implemeted as a dense matrix.
+data Linear s a b = Linear { getDenseMatrix :: HMat.Matrix s }
+
+
+instance (Num s) => Category (Linear s) where
+  type Object (Linear s) v = (VectorSpace v, Scalar v~s)
+
