83f475f38a59b44c490d9f86df74add7d8d34c0f
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2015-06-19 16:14:41 +0200

    Got something usable out of triangulation test case.

--- test/TriangT.hs
+++ test/TriangT.hs
@@ -7,17 +7,19 @@ import Data.SimplicialComplex
 import Graphics.Dynamic.Plot.R2
 import Diagrams.Prelude
 
-triangTest :: TriangT t Two R2 IO [[R2]]
+triangTest :: TriangT t Two R2 IO [Simplex Two R2]
 triangTest = do
    --[r0] <- simplexITList
    [s0,s1,s2] <- simplexITList
-   r1 <- introVertToTriang (1 ^& 1) [s0]
-   mapM (fmap simplexVertices' . lookSimplex) [r1]
+   introVertToTriang (1 ^& 1) [s0]
+   introVertToTriang ((-1) ^& 1) [s1]
+   introVertToTriang (1 ^& (-1)) [s2]
+   mapM lookSimplex =<< simplexITList
 
 main :: IO ()
 main = do
-   splxs <- evalTriangT triangTest . singleSimplex
-      $ 0^&0 :<| 0^&1 .<. 1^&0
+   splxs <- evalTriangT triangTest . singleSimplex $ 0^&0 :<| 0^&1 .<. 1^&0
+   print splxs
    return ()
    
 
