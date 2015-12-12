dd4e55db475f53bdb50c6c689dca7e0e00e40a43
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2015-12-12 18:56:30 +0100

    Start introducing a proper affine category.

---
+++ manifolds/Data/Function/Affine.hs
@@ -0,0 +1,151 @@
+-- |
+-- Module      : Data.Function.Affine
+-- Copyright   : (c) Justus Sagemüller 2015
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+{-# LANGUAGE FlexibleInstances        #-}
+{-# LANGUAGE UndecidableInstances     #-}
+{-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE FunctionalDependencies   #-}
+{-# LANGUAGE FlexibleContexts         #-}
+{-# LANGUAGE LiberalTypeSynonyms      #-}
+{-# LANGUAGE GADTs                    #-}
+{-# LANGUAGE RankNTypes               #-}
+{-# LANGUAGE TupleSections            #-}
+{-# LANGUAGE ConstraintKinds          #-}
+{-# LANGUAGE PatternGuards            #-}
+{-# LANGUAGE TypeOperators            #-}
+{-# LANGUAGE UnicodeSyntax            #-}
+{-# LANGUAGE MultiWayIf               #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
+{-# LANGUAGE RecordWildCards          #-}
+{-# LANGUAGE CPP                      #-}
+
+
+module Data.Function.Affine (
+              Affine(..)
+            ) where
+    
+
+
+import Data.List
+import Data.Maybe
+import Data.Semigroup
+
+import Data.VectorSpace
+import Data.LinearMap
+import Data.LinearMap.HerMetric
+import Data.MemoTrie (HasTrie(..))
+import Data.AffineSpace
+import Data.Basis
+import Data.Void
+import Data.Tagged
+import Data.Manifold.Types.Primitive
+import Data.Manifold.PseudoAffine
+
+import Data.CoNat
+import Data.VectorSpace.FiniteDimensional
+
+import qualified Prelude
+import qualified Control.Applicative as Hask
+
+import Control.Category.Constrained.Prelude hiding ((^))
+import Control.Arrow.Constrained
+import Control.Monad.Constrained
+import Data.Foldable.Constrained
+
+
+
+
+data Affine s d c
+   = Affine { affineCoOffset :: d
+            , affineOffset :: c
+            , affineSlope :: Needle d :-* Needle c
+            }
+
+
+instance (MetricScalar s) => Category (Affine s) where
+  type Object (Affine s) o = WithField s AffineManifold o
+  id = Affine zeroV zeroV idL
+  Affine cof aof slf . Affine cog aog slg
+      = Affine cog (aof .+^ lapply slf (aog.-.cof)) (slf*.*slg)
+
+{-
+
+instance (RealDimension s) => EnhancedCat (->) (Differentiable s) where
+  arr (Differentiable f) x = let (y,_,_) = f x in y
+
+instance (MetricScalar s) => Cartesian (Differentiable s) where
+  type UnitObject (Differentiable s) = ZeroDim s
+  swap = Differentiable $ \(x,y) -> ((y,x), lSwap, const zeroV)
+   where lSwap = linear swap
+  attachUnit = Differentiable $ \x -> ((x, Origin), lAttachUnit, const zeroV)
+   where lAttachUnit = linear $ \x ->  (x, Origin)
+  detachUnit = Differentiable $ \(x, Origin) -> (x, lDetachUnit, const zeroV)
+   where lDetachUnit = linear $ \(x, Origin) ->  x
+  regroup = Differentiable $ \(x,(y,z)) -> (((x,y),z), lRegroup, const zeroV)
+   where lRegroup = linear regroup
+  regroup' = Differentiable $ \((x,y),z) -> ((x,(y,z)), lRegroup, const zeroV)
+   where lRegroup = linear regroup'
+
+
+instance (MetricScalar s) => Morphism (Differentiable s) where
+  Differentiable f *** Differentiable g = Differentiable h
+   where h (x,y) = ((fx, gy), lPar, devfg)
+          where (fx, f', devf) = f x
+                (gy, g', devg) = g y
+                devfg δs = transformMetric lfst δx 
+                           ^+^ transformMetric lsnd δy
+                  where δx = devf $ transformMetric lcofst δs
+                        δy = devg $ transformMetric lcosnd δs
+                lPar = linear $ lapply f'***lapply g'
+         lfst = linear fst; lsnd = linear snd
+         lcofst = linear (,zeroV); lcosnd = linear (zeroV,)
+
+
+instance (MetricScalar s) => PreArrow (Differentiable s) where
+  terminal = Differentiable $ \_ -> (Origin, zeroV, const zeroV)
+  fst = Differentiable $ \(x,_) -> (x, lfst, const zeroV)
+   where lfst = linear fst
+  snd = Differentiable $ \(_,y) -> (y, lsnd, const zeroV)
+   where lsnd = linear snd
+  Differentiable f &&& Differentiable g = Differentiable h
+   where h x = ((fx, gx), lFanout, devfg)
+          where (fx, f', devf) = f x
+                (gx, g', devg) = g x
+                devfg δs = (devf $ transformMetric lcofst δs)
+                           ^+^ (devg $ transformMetric lcosnd δs)
+                lFanout = linear $ lapply f'&&&lapply g'
+         lcofst = linear (,zeroV); lcosnd = linear (zeroV,)
+
+
+instance (MetricScalar s) => WellPointed (Differentiable s) where
+  unit = Tagged Origin
+  globalElement x = Differentiable $ \Origin -> (x, zeroV, const zeroV)
+  const x = Differentiable $ \_ -> (x, zeroV, const zeroV)
+
+
+
+type DfblFuncValue s = GenericAgent (Differentiable s)
+
+instance (MetricScalar s) => HasAgent (Differentiable s) where
+  alg = genericAlg
+  ($~) = genericAgentMap
+instance (MetricScalar s) => CartesianAgent (Differentiable s) where
+  alg1to2 = genericAlg1to2
+  alg2to1 = genericAlg2to1
+  alg2to2 = genericAlg2to2
+instance (MetricScalar s)
+      => PointAgent (DfblFuncValue s) (Differentiable s) a x where
+  point = genericPoint
+-}
+
+
+
+
+
