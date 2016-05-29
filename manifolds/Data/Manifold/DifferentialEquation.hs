1ab00cac4bcaa371b243fa85f3b508cc505f0c45
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2016-05-29 03:21:46 +0200

    Come up with a module for defining differential equations.
    At the moment, only for linear case.

---
+++ manifolds/Data/Manifold/DifferentialEquation.hs
@@ -0,0 +1,80 @@
+-- |
+-- Module      : Data.Manifold.DifferentialEquation
+-- Copyright   : (c) Justus Sagemüller 2016
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveFoldable             #-}
+{-# LANGUAGE DeriveTraversable          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE ParallelListComp           #-}
+{-# LANGUAGE UnicodeSyntax              #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE PatternGuards              #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE LiberalTypeSynonyms        #-}
+
+
+module Data.Manifold.DifferentialEquation (
+              DifferentialEqn
+            , filterDEqnSolution_static, iterateFilterDEqn_static
+            ) where
+
+
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NE
+import Data.Semigroup
+
+import Data.VectorSpace
+import Data.LinearMap.HerMetric
+import Data.LinearMap.Category
+import Data.AffineSpace
+import Data.Basis
+
+import Data.Manifold.Types
+import Data.Manifold.PseudoAffine
+import Data.Function.Differentiable
+import Data.Function.Differentiable.Data
+import Data.Manifold.TreeCover
+import Data.Manifold.Web
+
+import qualified Numeric.LinearAlgebra.HMatrix as HMat
+
+import qualified Prelude as Hask hiding(foldl, sum, sequence)
+import qualified Control.Applicative as Hask
+import qualified Control.Monad       as Hask hiding(forM_, sequence)
+import qualified Data.Foldable       as Hask
+import qualified Data.Traversable as Hask
+
+import Control.Category.Constrained.Prelude hiding
+     ((^), all, elem, sum, forM, Foldable(..), foldr1, Traversable, traverse)
+import Control.Arrow.Constrained
+import Control.Monad.Constrained hiding (forM)
+import Data.Foldable.Constrained
+import Data.Traversable.Constrained (Traversable, traverse)
+
+
+constLinearDEqn :: (WithField ℝ LinearManifold x, WithField ℝ LinearManifold y)
+              => Linear ℝ (DualSpace y) (Linear ℝ y x) -> DifferentialEqn x y
+constLinearDEqn bwt = factoriseShade
+    >>> \(_x, Shade y δy) -> let j = bwt'm HMat.<\> (asPackedVector y)
+                                 δj = bwt' `transformMetric` recipMetric δy
+                             in Shade' (fromPackedVector j) δj
+ where bwt'@(DenseLinear bwt'm) = adjoint bwt
+
