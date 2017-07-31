332e8f6e40e1544abc9f8231eb20d3ef45cbbbaf
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-31 12:31:31 +0200

    Trivial test cases for manual web construction.

--- manifolds/test/tasty/test.hs
+++ manifolds/test/tasty/test.hs
@@ -9,20 +9,29 @@
 -- 
 module Main where
 
+import Data.Manifold.Types
+import Data.Manifold.TreeCover
 import Data.Manifold.Web
+import Data.Manifold.Web.Internal
+import Data.VectorSpace
+import Math.LinearMap.Category
 
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Data.Foldable (toList)
 
 
 main = defaultMain tests
 
-
-
 tests :: TestTree
 tests = testGroup "Tests"
-  [
+  [ testCase "Manually defining an empty web."
+    $ toList (fst $ toGraph (PointsWeb (PlainLeaves []) :: PointsWeb ℝ⁰ ())) @?= []
+  , testCase "Manually defining a single-point web."
+    $ toList (fst $ toGraph (PointsWeb (
+         PlainLeaves [ (zeroV, Neighbourhood () mempty euclideanNorm Nothing) ]
+       ) :: PointsWeb ℝ⁰ ())) @?= [[]]
   ]
 
 
