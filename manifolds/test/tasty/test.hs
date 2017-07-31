9faa950b3ab1ffbc35d678e336571b7b6f3440fe
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-31 15:50:11 +0200

    Failing test cases for traversal of simple webs.

--- manifolds/test/tasty/test.hs
+++ manifolds/test/tasty/test.hs
@@ -23,6 +23,8 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Data.Foldable (toList)
+import Control.Arrow
+import Control.Lens
 
 
 main = defaultMain tests
@@ -38,6 +40,17 @@ tests = testGroup "Tests"
     $ toList (fst $ toGraph triangularWeb) @?= [[1,2],[0,2],[0,1]]
   , testCase "Manually-defined simple quadratic web."
     $ toList (fst $ toGraph quadraticWeb) @?= [[1,2],[0,3],[0,3],[1,2]]
+  , testCase "Envi-aware traversal over simple quadratic web."
+    $ toList (fst . toGraph $ dummyWebFmap quadraticWeb) @?= [[1,2],[0,3],[0,3],[1,2]]
+  , testCase "Next-neighbours in empty web."
+    $ toList (nextNeighbours emptyWeb) @?= []
+  , testCase "Next-neighbours in single-point web."
+    $ toList (nextNeighbours singletonWeb) @?= [[]]
+  , testCase "Next-neighbours in simple triangular web."
+    $ toList (nextNeighbours triangularWeb) @?= [[]]
+  , testCase "Next-neighbours in simple quadratic web."
+    $ toList (nextNeighbours quadraticWeb) @?=
+     [[(1,()),(2,())],[(0,()),(3,())],[(0,()),(3,())],[(1,()),(2,())]]
   ]
  ]
 
@@ -66,3 +79,10 @@ quadraticWeb = PointsWeb $
 
 
 o = zeroV :: ℝ⁰
+
+dummyWebFmap :: PointsWeb ℝ⁰ a -> PointsWeb ℝ⁰ a
+dummyWebFmap = localFmapWeb $ \info -> info^.thisNodeData
+
+nextNeighbours :: PointsWeb ℝ⁰ a -> PointsWeb ℝ⁰ [(Int, a)]
+nextNeighbours = localFmapWeb $
+     \info -> second (\(_, ngb)->ngb^.thisNodeData) <$> info^.nodeNeighbours
