f94429826638d05c93c614642a560795666df550
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-02-16 18:17:11 +0100

    Genericise parallel transport over non-Hask categories.

--- manifolds/Data/Manifold/FibreBundle.hs
+++ manifolds/Data/Manifold/FibreBundle.hs
@@ -33,42 +33,42 @@ import Control.Arrow.Constrained
 import Data.Tagged
 
 
-class (PseudoAffine m, m ~ Interior m)
-           => ParallelTransporting m f where
-  parallelTransport :: m -> Needle m -> f -> f
+class (PseudoAffine m, m ~ Interior m, Category k)
+           => ParallelTransporting k m f where
+  parallelTransport :: m -> Needle m -> k f f
 
 instance (PseudoAffine m, m ~ Interior m, s ~ (Scalar (Needle m)))
-      => ParallelTransporting m (ZeroDim s) where
+      => ParallelTransporting (->) m (ZeroDim s) where
   parallelTransport _ _ Origin = Origin
 
-instance ParallelTransporting ℝ ℝ where
+instance ParallelTransporting (->) ℝ ℝ where
   parallelTransport _ _ x = x
 
-instance ParallelTransporting S⁰ ℝ⁰ where
+instance ParallelTransporting (->) S⁰ ℝ⁰ where
   parallelTransport _ _ x = x
-instance ParallelTransporting S¹ ℝ where
+instance ParallelTransporting (->) S¹ ℝ where
   parallelTransport _ _ x = x
-instance ParallelTransporting S² ℝ² where
+instance ParallelTransporting (->) S² ℝ² where
   parallelTransport p@(S² θ₀ φ₀) v x = case p.+~^v of
       S² θ₁ φ₁ -> undefined
 
-instance (ParallelTransporting a fa, ParallelTransporting b fb)
-              => ParallelTransporting (a,b) (fa,fb) where
+instance (ParallelTransporting (->) a fa, ParallelTransporting (->) b fb)
+              => ParallelTransporting (->) (a,b) (fa,fb) where
   parallelTransport (pa,pb) (va,vb) (xa,xb)
        = (parallelTransport pa va xa, parallelTransport pb vb xb)
 
-instance (ParallelTransporting a f, ParallelTransporting a g)
-              => ParallelTransporting a (f,g) where
+instance (ParallelTransporting (->) a f, ParallelTransporting (->) a g)
+              => ParallelTransporting (->) a (f,g) where
   parallelTransport p v (x,y)
        = (parallelTransport p v x, parallelTransport p v y)
 
 
-instance (ParallelTransporting m f, AdditiveGroup m, AdditiveGroup f)
+instance (ParallelTransporting (->) m f, AdditiveGroup m, AdditiveGroup f)
                 => AdditiveGroup (FibreBundle m f)
 
 instance ∀ m f .
-         ( ParallelTransporting m (Interior f), Semimanifold f
-         , ParallelTransporting (Needle m) (Needle f) )
+         ( ParallelTransporting (->) m (Interior f), Semimanifold f
+         , ParallelTransporting (->) (Needle m) (Needle f) )
                 => Semimanifold (FibreBundle m f) where
   type Interior (FibreBundle m f) = FibreBundle m (Interior f)
   type Needle (FibreBundle m f) = FibreBundle (Needle m) (Needle f)
@@ -86,9 +86,9 @@ instance ∀ m f .
       = FibreBundle (p.+~^v) (parallelTransport p v f.+~^δf)
 
 instance ∀ m f .
-         ( ParallelTransporting m f, ParallelTransporting m (Interior f)
+         ( ParallelTransporting (->) m f, ParallelTransporting (->) m (Interior f)
          , PseudoAffine f
-         , ParallelTransporting (Needle m) (Needle f) )
+         , ParallelTransporting (->) (Needle m) (Needle f) )
                 => PseudoAffine (FibreBundle m f) where
   pseudoAffineWitness = case ( pseudoAffineWitness :: PseudoAffineWitness m
                              , pseudoAffineWitness :: PseudoAffineWitness f ) of
