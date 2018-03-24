88bc039be5c46485dc107b69d390f1686a85396f
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-03-24 18:20:48 +0100

    More instances of simplices.

--- manifolds/Data/Simplex/Abstract.hs
+++ manifolds/Data/Simplex/Abstract.hs
@@ -13,6 +13,9 @@
 module Data.Simplex.Abstract where
 
 import Data.Manifold.Types.Primitive
+import Math.Manifold.Core.PseudoAffine
+
+import GHC.Generics
 
 data family AbstractSimplex v x
 
@@ -21,3 +24,14 @@ data instance AbstractSimplex ℝ  x = ℝ¹Simplex !x !x
 data instance AbstractSimplex ℝ² x = ℝ²Simplex !x !x !x
 data instance AbstractSimplex ℝ³ x = ℝ³Simplex !x !x !x !x
 data instance AbstractSimplex ℝ⁴ x = ℝ⁴Simplex !x !x !x !x !x
+
+data instance AbstractSimplex (ℝ, v) x = ConeSimplex !x !(AbstractSimplex v x)
+
+newtype instance AbstractSimplex (GenericNeedle m) x
+       = GenericSimplex (AbstractSimplex (Rep m ()) x)
+
+newtype instance AbstractSimplex (NeedleProductSpace f g p) x
+         = GenProdSimplex (AbstractSimplex (Needle (f p), Needle (g p)) x)
+
+
+type Simplex m = AbstractSimplex (Needle m) m
