429601e1565ae26caa8a717327d79178d23f6354
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-03-24 23:32:10 +0100

    Functor etc. instances for abstract simplices.

--- manifolds/Data/Simplex/Abstract.hs
+++ manifolds/Data/Simplex/Abstract.hs
@@ -9,29 +9,68 @@
 -- 
 
 {-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE StandaloneDeriving       #-}
+{-# LANGUAGE DeriveFunctor            #-}
+{-# LANGUAGE DeriveFoldable           #-}
+{-# LANGUAGE DeriveTraversable        #-}
+{-# LANGUAGE UndecidableInstances     #-}
+{-# LANGUAGE FlexibleContexts         #-}
+{-# LANGUAGE FlexibleInstances        #-}
 
 module Data.Simplex.Abstract where
 
 import Data.Manifold.Types.Primitive
 import Math.Manifold.Core.PseudoAffine
+import Data.Manifold.PseudoAffine
+
+import Data.Traversable
 
 import GHC.Generics
 
 data family AbstractSimplex v x
 
 data instance AbstractSimplex ℝ⁰ x = ℝ⁰Simplex !x
-data instance AbstractSimplex ℝ  x = ℝ¹Simplex !x !x
+ deriving (Functor, Foldable, Traversable)
+instance Applicative (AbstractSimplex ℝ⁰) where
+  pure = ℝ⁰Simplex
+  ℝ⁰Simplex p <*> ℝ⁰Simplex q = ℝ⁰Simplex $ p q
+
+data instance AbstractSimplex ℝ  x = ℝSimplex !x !x
+ deriving (Functor, Foldable, Traversable)
+data instance AbstractSimplex ℝ¹ x = ℝ¹Simplex !x !x
+ deriving (Functor, Foldable, Traversable)
+
 data instance AbstractSimplex ℝ² x = ℝ²Simplex !x !x !x
+ deriving (Functor, Foldable, Traversable)
+
 data instance AbstractSimplex ℝ³ x = ℝ³Simplex !x !x !x !x
+ deriving (Functor, Foldable, Traversable)
+
 data instance AbstractSimplex ℝ⁴ x = ℝ⁴Simplex !x !x !x !x !x
+ deriving (Functor, Foldable, Traversable)
 
 data instance AbstractSimplex (ℝ, v) x = ConeSimplex !x !(AbstractSimplex v x)
+deriving instance (Functor (AbstractSimplex v)) => Functor (AbstractSimplex (ℝ,v))
+deriving instance (Foldable (AbstractSimplex v)) => Foldable (AbstractSimplex (ℝ,v))
+deriving instance (Traversable (AbstractSimplex v)) => Traversable (AbstractSimplex (ℝ,v))
 
 newtype instance AbstractSimplex (GenericNeedle m) x
        = GenericSimplex (AbstractSimplex (Rep m ()) x)
+deriving instance (Functor (AbstractSimplex (Rep m ())))
+         => Functor (AbstractSimplex (GenericNeedle m))
+deriving instance (Foldable (AbstractSimplex (Rep m ())))
+         => Foldable (AbstractSimplex (GenericNeedle m))
+deriving instance (Traversable (AbstractSimplex (Rep m ())))
+         => Traversable (AbstractSimplex (GenericNeedle m))
 
 newtype instance AbstractSimplex (NeedleProductSpace f g p) x
          = GenProdSimplex (AbstractSimplex (Needle (f p), Needle (g p)) x)
+deriving instance (Functor (AbstractSimplex (Needle (f p), Needle (g p))))
+         => Functor (AbstractSimplex (NeedleProductSpace f g p))
+deriving instance (Foldable (AbstractSimplex (Needle (f p), Needle (g p))))
+         => Foldable (AbstractSimplex (NeedleProductSpace f g p))
+deriving instance (Traversable (AbstractSimplex (Needle (f p), Needle (g p))))
+         => Traversable (AbstractSimplex (NeedleProductSpace f g p))
 
 
 type Simplex m = AbstractSimplex (Needle m) m
