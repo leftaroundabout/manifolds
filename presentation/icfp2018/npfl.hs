68d21ad211f31403ca7f721c03a99cecf8ac1df1
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-09-09 13:03:31 +0200

    New subproject for the presentation I want to hold at ICFP.

---
+++ presentation/icfp2018/npfl.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+import Presentation.Yeamer
+import Presentation.Yeamer.Maths
+import Math.LaTeX.StringLiterals
+import qualified Text.Blaze.Html as Blaze
+import Text.Cassius
+import Data.Semigroup.Numbered
+
+import Graphics.Dynamic.Plot.R2
+import qualified Diagrams.Prelude as Dia
+import qualified Diagrams.Backend.Cairo as Dia
+
+import System.Environment
+import Control.Lens
+import Control.Concurrent
+
+
+main :: IO ()
+main = yeamer . styling style $ do
+   "Manifolds"
+     ====== do
+      ""
+       
+
+style = [cassius|
+   body
+     height: 100vh
+     color: #ffe
+     background: linear-gradient(#263, #516)
+     font-size: 6vmin
+     font-family: "Linux libertine", "Times New Roman"
+   .main-title
+     font-size: 180%
+   h1
+     font-size: 150%
+   div
+     width: 95%
+     height: 95%
+     text-align: center
+     margin: auto
+     border-radius: 4px
+     border:1px solid #000;
+     background: rgba(0,0,15,0.25);
+   .headed-container
+     height: 80%
+   .vertical-concatenation
+     display: flex
+     flex-direction: column
+   .emph
+     font-style: italic
+   .small
+     font-size: 67%
+   .verb
+     display: inline-block
+     font-size: 86%
+     background-color: #227
+     font-family: "Ubuntu Mono", "Droid Sans mono", "Courier New"
+   pre
+     text-align: left
+     font-size: 86%
+     background-color: #204
+     font-family: "Ubuntu Mono", "Droid Sans mono", "Courier New"
+  |] ()
+
+plotServ :: [DynamicPlottable] -> Presentation -> Presentation
+plotServ pl cont = serverSide (forkIO (plotWindow pl >> return ()) >> return ())
+                     >> cont
+
+plotStat :: ViewportConfig -> [DynamicPlottable] -> Presentation
+plotStat viewCfg pl = imageFromFileSupplier "png" $ \file -> do
+    prerendered <- plotPrerender viewCfg pl
+    Dia.renderCairo file
+                    (Dia.mkSizeSpec $ Just (fromIntegral $ viewCfg^.xResV)
+                               Dia.^& Just (fromIntegral $ viewCfg^.yResV))
+                    prerendered
