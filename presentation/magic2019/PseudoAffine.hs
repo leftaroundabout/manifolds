115805af6a6a8d207ab16b681c5545ce1dd70a92
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2019-02-27 04:43:11 +0100

    More points for neighbour search.

--- presentation/magic2019/PseudoAffine.hs
+++ presentation/magic2019/PseudoAffine.hs
@@ -474,9 +474,10 @@ main = do
                                               candidates of
                                  (i,_) -> (i,ev^.releaseLocation)
                                             : filter ((/=i).fst) candidates )
-                             [               (1, (0,-1)), (2, (1,-1))
-                             , (3, (-1,0)),               (4, (1,0))
-                             , (5, (-1,1)),  (6, (0,1)),  (7, (1,1)) ]
+                             (zip[1..]
+                               [ (-2,-1), (-1,-1), ( 0,-1), ( 1,-1), ( 2,-1)
+                               , (-2, 0), (-1, 0),{-ORIGIN-}( 1, 0), ( 2, 0)
+                               , (-2, 1), (-1, 1), ( 0, 1), ( 1, 1), ( 2, 1) ])
                     , unitAspect, dynamicAxes
                     ]
        ,"Divide the data with a tree algorithm, to avoid "<>𝑂°𝑛◝2$<>" cost."
