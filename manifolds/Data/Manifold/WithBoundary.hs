ac545f938585f9d45256f72c66c645f9305ddcb6
Justus Sagemüller <jsag@hvl.no>
2020-10-06 23:56:48 +0200

    Propose a new, dedicated class for manifolds with boundary.

---
+++ manifolds/Data/Manifold/WithBoundary.hs
@@ -0,0 +1,67 @@
+-- |
+-- Module      : Math.Manifold.Core.PseudoAffine
+-- Copyright   : (c) Justus Sagemüller 2016
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsag $ hvl.no
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+{-# LANGUAGE FlexibleInstances        #-}
+{-# LANGUAGE UndecidableInstances     #-}
+{-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE FlexibleContexts         #-}
+{-# LANGUAGE GADTs                    #-}
+{-# LANGUAGE DefaultSignatures        #-}
+{-# LANGUAGE DeriveGeneric            #-}
+{-# LANGUAGE StandaloneDeriving       #-}
+{-# LANGUAGE UnicodeSyntax            #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
+{-# LANGUAGE TypeOperators            #-}
+{-# LANGUAGE TypeInType               #-}
+
+
+module Data.Manifold.WithBoundary where
+
+import Data.VectorSpace
+import Data.AffineSpace
+import Data.Basis
+
+import Math.Manifold.Core.PseudoAffine
+import Data.Manifold.Types.Primitive
+import Math.Manifold.VectorSpace.ZeroDimensional
+
+import Control.Applicative
+import Control.Arrow
+
+import qualified GHC.Generics as Gnrx
+import GHC.Generics (Generic, (:*:)(..))
+import Data.Kind (Type)
+
+import Data.CallStack (HasCallStack)
+
+
+class VectorSpace (FullSubspace h) => HalfSpace h where
+  type FullSubspace h :: Type
+  scaleNonNeg :: ℝay -> h -> h
+  fromFullSubspace :: FullSubspace h -> h
+  projectToFullSubspace :: h -> FullSubspace h
+
+instance HalfSpace ℝay where
+  type FullSubspace ℝay = ℝ⁰
+  scaleNonNeg (Cℝay μ Origin) (Cℝay l Origin) = Cℝay (μ*l) Origin
+  fromFullSubspace o = Cℝay 0 o
+  projectToFullSubspace (Cℝay _ o) = o
+
+class ( Semimanifold (Interior m), Semimanifold (Boundary m)
+      , HalfSpace (HalfNeedle m), FullSubspace (HalfNeedle m) ~ Needle (Boundary m)
+      ) => BoundedSemimanifold m where
+  type Interior m :: Type
+  type Boundary m :: Type
+  type HalfNeedle m :: Type
+  fromInterior :: Interior m -> m
+  toInterior :: m -> Maybe (Interior m)
+  (|+^) :: Boundary m -> HalfNeedle m -> m
+  (.-|) :: m -> Boundary m -> HalfNeedle m
+
