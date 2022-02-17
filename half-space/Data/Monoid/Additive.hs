11a8e8ab4b787a536cb2a59ed3300682cb35d515
Justus Sagemüller <jsag@hvl.no>
2022-02-17 13:12:50 +0100

    Generic/standard instances for `AdditiveMonoid`.

--- half-space/Data/Monoid/Additive.hs
+++ half-space/Data/Monoid/Additive.hs
@@ -28,29 +28,71 @@
 {-# LANGUAGE CPP                      #-}
 
 
-module Data.Monoid.Additive where
+module Data.Monoid.Additive (AdditiveMonoid(..), HalfSpace(..)) where
 
 import Data.VectorSpace
 import Data.AffineSpace
-import Data.Basis
+import Data.Int
+import Data.Word
 
 import Math.Manifold.Core.PseudoAffine
 import Math.Manifold.Core.Types
 import Math.Manifold.VectorSpace.ZeroDimensional
 import Control.Applicative
 import Control.Arrow
+import Data.Void
 
 import qualified GHC.Generics as Gnrx
 import GHC.Generics (Generic, (:*:)(..))
 import Data.Kind (Type)
 import Proof.Propositional (Empty(..))
 
-import Data.CallStack (HasCallStack)
 
+
+type AMRep h = Gnrx.Rep h Void
 
 class AdditiveMonoid h where
   zeroHV :: h
+  default zeroHV :: (Generic h, AdditiveMonoid (AMRep h)) => h
+  zeroHV = Gnrx.to (zeroHV :: AMRep h)
   addHVs :: h -> h -> h
+  default addHVs :: (Generic h, AdditiveMonoid (AMRep h)) => h -> h -> h
+  addHVs p q = Gnrx.to (addHVs (Gnrx.from p) (Gnrx.from q) :: AMRep h)
+
+instance AdditiveMonoid h => AdditiveMonoid (Gnrx.Rec0 h s) where
+  zeroHV = Gnrx.K1 zeroHV
+  addHVs (Gnrx.K1 p) (Gnrx.K1 q) = Gnrx.K1 $ addHVs p q
+instance AdditiveMonoid (f p) => AdditiveMonoid (Gnrx.M1 i c f p) where
+  zeroHV = Gnrx.M1 zeroHV
+  addHVs (Gnrx.M1 p) (Gnrx.M1 q) = Gnrx.M1 $ addHVs p q
+instance (AdditiveMonoid (f p), AdditiveMonoid (g p))
+            => AdditiveMonoid ((f:*:g) p) where
+  zeroHV = zeroHV :*: zeroHV
+  addHVs (x:*:y) (ξ:*:υ) = addHVs x ξ :*: addHVs y υ
+
+#define AdditiveGroupMonoid(g)     \
+instance AdditiveMonoid (g) where { \
+  zeroHV = zeroV;                    \
+  addHVs = (^+^) }
+
+#define NumAdditiveMonoid(g)       \
+instance AdditiveMonoid (g) where { \
+  zeroHV = 0;                        \
+  addHVs = (+) }
+
+NumAdditiveMonoid(Int)
+NumAdditiveMonoid(Integer)
+NumAdditiveMonoid(Float)
+NumAdditiveMonoid(Double)
+NumAdditiveMonoid(Int16)
+NumAdditiveMonoid(Int32)
+NumAdditiveMonoid(Int64)
+NumAdditiveMonoid(Word16)
+NumAdditiveMonoid(Word32)
+NumAdditiveMonoid(Word64)
+
+instance (AdditiveMonoid h, AdditiveMonoid i) => AdditiveMonoid (h,i)
+instance (AdditiveMonoid h, AdditiveMonoid i, AdditiveMonoid j) => AdditiveMonoid (h,i,j)
 
 class AdditiveMonoid h => HalfSpace h where
   type FullSubspace h :: Type
