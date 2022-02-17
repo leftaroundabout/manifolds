a32cad30d6ad055bfff76b2aea7966e314afe10f
Justus Sagemüller <jsag@hvl.no>
2022-02-17 14:09:23 +0100

    Towards making also `HalfSpace` generic-derivable.

--- half-space/Data/Monoid/Additive.hs
+++ half-space/Data/Monoid/Additive.hs
@@ -84,9 +84,12 @@ NumAdditiveMonoid(Int)
 NumAdditiveMonoid(Integer)
 NumAdditiveMonoid(Float)
 NumAdditiveMonoid(Double)
+NumAdditiveMonoid(Int8)
 NumAdditiveMonoid(Int16)
 NumAdditiveMonoid(Int32)
 NumAdditiveMonoid(Int64)
+NumAdditiveMonoid(Word)
+NumAdditiveMonoid(Word8)
 NumAdditiveMonoid(Word16)
 NumAdditiveMonoid(Word32)
 NumAdditiveMonoid(Word64)
@@ -96,15 +99,55 @@ instance (AdditiveMonoid h, AdditiveMonoid i, AdditiveMonoid j) => AdditiveMonoi
 
 class AdditiveMonoid h => HalfSpace h where
   type FullSubspace h :: Type
+  type FullSubspace h = GenericFullSubspace h
   type Ray h :: Type
-  type Ray h = Cℝay ℝ⁰
+  type Ray h = Ray (AMRep h)
   scaleNonNeg :: Ray h -> h -> h
+  default scaleNonNeg :: ( Generic h, HalfSpace (AMRep h)
+                         , FullSubspace h ~ GenericFullSubspace h
+                         , Ray h ~ Ray (AMRep h) )
+             => Ray h -> h -> h
+  scaleNonNeg μ p = Gnrx.to (scaleNonNeg μ (Gnrx.from p) :: AMRep h)
   fromFullSubspace :: FullSubspace h -> h
+  default fromFullSubspace :: ( Generic h, HalfSpace (AMRep h)
+                              , FullSubspace h ~ GenericFullSubspace h
+                              , Ray h ~ Ray (AMRep h) )
+             => FullSubspace h -> h
+  fromFullSubspace (GenericFullSubspace x) = Gnrx.to (fromFullSubspace x :: AMRep h)
   projectToFullSubspace :: h -> FullSubspace h
   fullSubspaceIsVectorSpace :: (VectorSpace (FullSubspace h) => r) -> r
   default fullSubspaceIsVectorSpace :: VectorSpace (FullSubspace h)
                               => (VectorSpace (FullSubspace h) => r) -> r
   fullSubspaceIsVectorSpace q = q
+
+newtype GenericFullSubspace h = GenericFullSubspace
+    { getGenericFullSubspace :: FullSubspace (AMRep h) }
+  deriving (Generic)
+instance AdditiveGroup (FullSubspace (AMRep h)) => AdditiveGroup (GenericFullSubspace h)
+instance VectorSpace (FullSubspace (AMRep h)) => VectorSpace (GenericFullSubspace h)
+
+instance ∀ h s . HalfSpace h => HalfSpace (Gnrx.Rec0 h s) where
+  type FullSubspace (Gnrx.Rec0 h s) = FullSubspace h
+  type Ray (Gnrx.Rec0 h s) = Ray h
+  scaleNonNeg μ (Gnrx.K1 p) = Gnrx.K1 $ scaleNonNeg μ p
+  fullSubspaceIsVectorSpace c = fullSubspaceIsVectorSpace @h c
+  fromFullSubspace x = Gnrx.K1 $ fromFullSubspace x
+  projectToFullSubspace (Gnrx.K1 p) = projectToFullSubspace p
+instance HalfSpace (f p) => HalfSpace (Gnrx.M1 i c f p) where
+  type FullSubspace (Gnrx.M1 i c f p) = FullSubspace (f p)
+  type Ray (Gnrx.M1 i c f p) = Ray (f p)
+  scaleNonNeg μ (Gnrx.M1 p) = Gnrx.M1 $ scaleNonNeg μ p
+  fullSubspaceIsVectorSpace c = fullSubspaceIsVectorSpace @(f p) c
+  fromFullSubspace x = Gnrx.M1 $ fromFullSubspace x
+  projectToFullSubspace (Gnrx.M1 p) = projectToFullSubspace p
+
+data GenericProductFullSubspace f g p
+   = GenericProductFullSubspace { lFullSubspace :: !(FullSubspace (f p))
+                                , rFullSpace :: !(g p) }
+
+-- instance (HalfSpace (f p), VectorSpace (g p), Scalar (Ray (f p)), FullScalar) => HalfSpace ((f:*:g) p) where
+--   type FullSubspace ((f:*:g) p) = GenericProductFullSubspace f g p
+--   type Ray (Gnrx.Rec0 h s) = Ray h
 
 instance AdditiveMonoid (ZeroDim k) where
   zeroHV = Origin
@@ -115,3 +158,23 @@ instance HalfSpace (ZeroDim k) where
   fromFullSubspace _ = Origin
   projectToFullSubspace Origin = Origin
 
+#define WordHalfSpace(w)             \
+instance HalfSpace (w) where {        \
+  type FullSubspace (w) = ZeroDim (w); \
+  type Ray (w) = (w);                   \
+  scaleNonNeg = (*);                     \
+  fromFullSubspace Origin = 0;            \
+  projectToFullSubspace _ = Origin }
+
+WordHalfSpace(Word)
+WordHalfSpace(Word8)
+WordHalfSpace(Word16)
+WordHalfSpace(Word32)
+WordHalfSpace(Word64)
+
+
+newtype GnTestHS = GnTestHS Word
+  deriving (Generic)
+
+instance AdditiveMonoid GnTestHS
+instance HalfSpace GnTestHS
